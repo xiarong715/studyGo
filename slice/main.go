1c03fb5d99114661c881e7b8aaf1615e7977ab1e
xiarong <xiarong@ipanel.cn>
2023-05-30 17:41:32 +0800

    slice

--- slice/main.go
+++ slice/main.go
@@ -1,12 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 // 切片是引用类型。
 // 引用类型：引用类型的变量中存的是地址，地址指向堆。
 // Go中没有引用传递，只有值传递，变量的值为真实的值或为一个地址。
 // Go中的引用类型：指针、map、slice、接口、函数。
 // 在切片引用的底层数组中，从切片的第1个元素开始到数组的最后一个元素的长度就是切片的长度。
+
+// 值传递
+func modifySlice(s []int) {
+	s = append(s, 4)
+	s[0] = 10 // 修改的是切片最新指向的数组
+} // 所以：只要对切片追加元素，都要返回最新的切片，避免出现bug
+
+func modifySlice2(s *[]int) {
+	*s = append(*s, 4)
+	(*s)[0] = 10
+}
 
 func main() {
 	s1 := []string{"hello", "world", "lsy"}
@@ -95,10 +109,49 @@ func main() {
 	// copy 复制切片
 	// 因为切片是引用类型，因为通过复制，切断两切片的关联
 	s15 := []int{1, 2, 3}
-	s16 := s15 // 引用同一个内存地址
+	s16 := s15 // 引用同一个内存地址，s15和s16有关联关系
 	fmt.Println(s16)
 
 	s17 := []int{2, 3, 4}
 	s18 := make([]int, 3)
-	copy(s18, s17) // 把s17的内容复制到s18中
+	copy(s18, s17) // 把s17的内容复制到s18中，s17和s18没关联关系，这也是copy的意义
+
+	// 删除元素
+	s19 := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	fmt.Println("del before: ", s19)
+	s19 = append(s19[:3], s19[4:]...) // 删除下标为3的元素
+	fmt.Println("del after: ", s19)
+
+	// 数组转切片
+	s20 := [...]int{1, 2, 3, 4, 5} // 数组
+	s21 := s20[:]                  // 对数组切片
+	fmt.Println(s21)
+	fmt.Printf("s20 type: %T, s21 type: %T\n", s20, s21) // s20 type: [5]int, s21 type: []int
+
+	// exam
+	s22 := make([]int, 5, 10)
+	for i := 0; i < 10; i++ {
+		s22 = append(s22, i)
+	}
+	fmt.Println(s22)                                   // 0 0 0 0 0 0 1 2 3 4 5 6 7 8 9
+	fmt.Printf("%p %p %p\n", s22[:], s22[1:], s22[2:]) // 0xc0000c2000 0xc0000c2008 0xc0000c2010  每个元素占8个字节，元素之间的地址相邻
+
+	// 直接定义与make定义的区别
+	var s []int          // 没给切片分配底层数组，指针字段指向nil
+	ss := make([]int, 0) // 分配了底层数组，只不过是包含0个元素的内存空间
+	fmt.Println(s, ss)
+
+	res1, _ := json.Marshal(s)
+	res2, _ := json.Marshal(ss)
+	res3, _ := json.Marshal([]int{1, 2, 3})
+	fmt.Println(string(res1)) // 注意：是null
+	fmt.Println(string(res2)) // []
+	fmt.Println(string(res3)) // [1, 2, 3]
+
+	sss := []int{1, 2, 3}
+	modifySlice(sss) // 在函数中追加元素后，扩容了，底层数组改变了。  但sss指向的底层数组没变。因此只要对切片做append的操作，都要返回新的切片
+	fmt.Println(sss) // [1 2 3]
+
+	modifySlice2(&sss) // 传切片的地址，可以保证正确修改切片的值。和C语言类似，传指针，对正确的改变切片的值。
+	fmt.Println(sss)   // [10 2 3 4]
 }
